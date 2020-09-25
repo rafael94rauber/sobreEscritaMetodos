214575ad6b3c8a009c1aae7dd8f1801261b87ac7
Rafael Eduardo Rauber <rrauber@bempromotora.com.br>
2020-09-25 12:47:49 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sobreEscritaMetodos
+ sobreEscritaMetodos
---
+++ sobreEscritaMetodos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace sobreEscritaMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teste = new teste();
+            teste.ToString();
+            Console.WriteLine("Hello World!");
+
+        }        
+    }
+}
